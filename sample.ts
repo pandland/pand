7043ab21d148b22525c88456ce25badcd8faa34f
Michał Dziuba <mail@michaldziuba.dev>
2024-08-02 01:26:59 +0200

    add more complex sample

--- sample.ts
+++ sample.ts
@@ -4,13 +4,29 @@ interface Socket {
   close: () => void;
 }
 
+function getPathFromRequest(chunk: string) {
+  const line = chunk.split('\r\n')[0];
+  if (!line) {
+    return "Invalid";
+  }
+
+  const path = line.split(' ')[1];
+  if (!path) {
+    return "Invalid";
+  }
+
+  return path;
+}
+
 tcpListen((socket: Socket) => {
   println("Client connected");
 
-  socket.read((chuk) => {
-    println(`Received data: ${chuk}`);
-    socket.write("HTTP/1.1 200 OK\r\nContent-Length: 21\r\nContent-Type: text/html\r\nConnection: close\r\n");
-    socket.write("\r\n<h1>Hello world!</h1>");
+  socket.read((chunk) => {
+    const path = getPathFromRequest(chunk);
+    println(`Received data:\n${path}`);
+    const response = `<h1>Path: ${path}</h1>`;
+    socket.write(`HTTP/1.1 200 OK\r\nContent-Length: ${response.length}\r\nContent-Type: text/html\r\nConnection: close\r\n\r\n`);
+    socket.write(response);
     socket.close();
   });
 }, 8000);
