b9e59e27de0b7db8cd1f6ae38f4332b992153b86
Michał Dziuba <mail@michaldziuba.dev>
2024-09-26 02:57:32 +0200

    build: improve cmake file

--- core/main.cc
+++ core/main.cc
@@ -7,8 +7,8 @@
 int main(int argc, char* argv[]) {
     signal(SIGPIPE, SIG_IGN);
     const char* flags = "--max-old-space-size=4096";
+    v8::V8::InitializeICU();
     v8::V8::SetFlagsFromString(flags);
-
     v8::V8::InitializeICUDefaultLocation(argv[0]);
     v8::V8::InitializeExternalStartupData(argv[0]);
 
