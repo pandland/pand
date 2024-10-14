18146967e28a735b00d264fd4ef6ca4e895fbdc8
Michał Dziuba <mail@michaldziuba.dev>
2024-10-14 04:46:51 +0200

    minor changes in timer

--- core/timer.cc
+++ core/timer.cc
@@ -1,5 +1,16 @@
+#include "pand.h"
+#include "timer.h"
+
 namespace pand::core {
 
+void Timer::initialize(v8::Local<v8::Object> exports) {}
 
+void Timer::constructor(const v8::FunctionCallbackInfo<v8::Value> &args) {}
+
+void Timer::setInterval(const v8::FunctionCallbackInfo<v8::Value> &args) {}
+
+void Timer::setTimeout(const v8::FunctionCallbackInfo<v8::Value> &args) {}
+
+void Timer::clear(const v8::FunctionCallbackInfo<v8::Value> &args) {}
 
 }
--- core/timer.h
+++ core/timer.h
@@ -1,30 +1,34 @@
 #pragma once
-#include <v8.h>
+#include "pand.h"
 #include <pandio.h>
+#include <v8.h>
 
 namespace pand::core {
 
 class Timer {
-
-  enum TimerType {
-    INTERVAL,
-    TIMEOUT
-  };
+  enum class Type { INTERVAL, TIMEOUT };
 
   int id;
   pd_timer_t handle;
-  TimerType type;
+  Timer::Type type;
 
 public:
-  // JS API
+  Timer(int id, Timer::Type type) : id(id), type(type) {
+    pd_timer_init(Pand::get()->ctx, &handle);
+  }
+
+  static int counter;
+
+  // JS API:
   static void initialize(v8::Local<v8::Object> exports);
 
-  static void setTimeout(const v8::FunctionCallbackInfo<v8::Value>& args);
+  static void constructor(const v8::FunctionCallbackInfo<v8::Value> &args);
 
-  static void setInterval(const v8::FunctionCallbackInfo<v8::Value>& args);
+  static void setTimeout(const v8::FunctionCallbackInfo<v8::Value> &args);
 
-  static void clear(const v8::FunctionCallbackInfo<v8::Value>& args);
+  static void setInterval(const v8::FunctionCallbackInfo<v8::Value> &args);
 
+  static void clear(const v8::FunctionCallbackInfo<v8::Value> &args);
 };
 
-}
+} // namespace pand::core
