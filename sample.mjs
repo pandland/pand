24fb5c5714704a344f1ea6405a58e6e17e658c10
Michał Dziuba <mail@michaldziuba.dev>
2024-11-17 03:30:21 +0100

    chore: delete sample.mjs

--- sample.mjs
+++
@@ -1,8 +0,0 @@
-import { open, O_RDONLY } from 'std:fs';
-
-const file = await open("../test.md", O_RDONLY);
-const buf = new Buffer(1024);
-const size = await file.read(buf);
-console.log('size:', size);
-console.log(buf);
-await file.close();
